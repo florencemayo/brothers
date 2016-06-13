403764bf6ecb1a7d8e614f8e650d22566abc849b
florencemayo <florencemayo11@gmail.com>
2016-06-14 00:28:14 +0200

    edit, add, delete functionalities are working

--- app/brother-list/brother-list.component.js
+++ app/brother-list/brother-list.component.js
@@ -4,41 +4,71 @@ var myModule = angular.module('brotherList');
 
 myModule.component('brotherList', {
 		templateUrl: 'brother-list/brother-list.template.htm',
-		controller: ['BrotherFactory', '$scope', //'Brother',
-					  function BrotherListController(BrotherFactory,$scope){
+		controller: ['BrotherFactory', '$scope', '$localstorage',
+					  function BrotherListController(BrotherFactory,$scope, $localstorage){
+				 	     //set the initial variable to sort
+				 	     this.categories='age';
+
 				 	     $scope.newBrother = {};
+				 	     
+				 	      //INITIALIZE LOCAL STORAGE
+				 	    //uncomment to initialize 
+				 	    //$localstorage.setObject("bros",[]);
 
-				 	    //this.brothers = Brother.query();
-				 		this.categories='age';
-				 		$scope.bros;
-				 		BrotherFactory.getBros().success(function(data){
+				 	     //initialize bros
+				 	    $scope.bros=$localstorage.getObject('bros').length> 0 ?$localstorage.getObject('bros'):[];
+
+				 	   console.log($localstorage.getObject('bros'));
+
+				 	    //var list=$localstorage.getObject("bros");
+				 		//$scope.bros = $localstorage.formatData(list);
+
+			         	//get the JSON data from brothers.json
+			         	/*
+			         	BrotherFactory.getBros().success(function(data){
 			         		$scope.bros = data;
-			         		//console.log($scope.bros);
-			            }).error(function(error){
+			         	}).error(function(error){
 			         		console.log(error);
-			         });
-
+			         });*/ 
+					
 					//function to add brother details
 			         $scope.inputBrother = function(newBrother){
 			         	newBrother.imageUrl="img/default.jpg";
-			         	$scope.bros.push(newBrother);
+			         	newBrother.id=$scope.bros.length;
+						$scope.bros.push(newBrother);
+					    if($scope.bros.length>0) {$localstorage.setObject('bros',$scope.bros);}
+			  			
+			         	console.log($localstorage.getObject('bros'));
+			         	//UPDATE A LOCAL STORAGE
+			         	/*$localstorage.setObject("Bros", JSON.stringify($scope.bros));
+			         	
+					    bross = JSON.parse($localstorage.getObject("Bros"));
+					    console.log(bross.length)
 
-			         	$scope.newBrother = {};
+						//reset the input fields
+			         	$scope.newBrother = {};*/
 			         };
-			         
-			         //function to set the field when selecting a brother
+
+			        //function to set the field when selecting a brother
 			         $scope.selectBrother = function(brother){
+			         	var index = $scope.bros.indexOf(brother);
 			         	//pop-down the edit window
 			         	$scope.editBrother =true;
 			         	$scope.addBrother =false;
 			         	//populate the edit fields
-			         	$scope.editedBrother = brother;
+			         	$scope.editedBrother = $scope.bros[index];
 			         };
 
-			         $scope.saveEditedBrother = function(){
-			         	$scope.editedBrother ={};
+			         $scope.saveEditedBrother = function(brother){
+			         	var index = $scope.bros.indexOf(brother);
+						
 			         	$scope.editBrother =false;
-					};
+			         	$scope.bros[index]=$scope.editedBrother;
+			         	if($scope.bros.length>0) {$localstorage.setObject('bros',$scope.bros);}
+
+			         	$scope.editedBrother ={};
+			         	
+			  		};
 
 					$scope.deleteBrother= function(brother){
 						//get the indexof the listing in an array
@@ -46,6 +76,8 @@ myModule.component('brotherList', {
 						$scope.bros.splice(index, 1);
 						$scope.editedBrother ={};
 						$scope.editBrother =false;
+						if($scope.bros.length>0) {$localstorage.setObject('bros',$scope.bros);}
+			  			
 					};
 			}
 		]
--- app/core/brotherFactory.js
+++ app/core/brotherFactory.js
@@ -1,8 +1,8 @@
 'use strict';
 /* IMPLEMENTATION WITH HTTP SERVICE*/
 
-angular.module('core.brother').
-	factory('BrotherFactory', function($http){
+angular.module('core.brother')
+	.factory('BrotherFactory', function($http){
 		
 		function getBrothers(){
 			return $http.get('brothers/brothers.json');
@@ -11,5 +11,28 @@ angular.module('core.brother').
 		return {
 			getBros: getBrothers
 		};
-	}
-);
+	})
+
+	.factory('$localstorage', ['$window', function($window) {
+	  	return {
+	    set: function(key, value) {
+	        $window.localStorage[key] = value;
+	    },
+	    get: function(key, defaultValue) {
+	      return $window.localStorage[key] || defaultValue;
+	    },
+	    setObject: function(key, value) {
+	      $window.localStorage[key] = JSON.stringify(value);
+	    },
+	    getObject: function(key) {
+	      return JSON.parse($window.localStorage[key] || '{}');
+	    },
+	    formatData : function(json){
+	    	var array=[];
+	    	for(var key in json){
+	    		array.push(json[key]);
+	    	}
+	    	return array;
+	    }
+	  }
+}]);
